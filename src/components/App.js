66fbacf28d05347ec7402b03d0ceaeafbd5ddb33
voyagerdev26 <devs251898@gmail.com>
2026-05-10 17:53:38 +0530

    version One

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,23 @@
 
 import React from "react";
 import './../styles/App.css';
+import Navigation from "./Navigation";
+import Home from "./Home";
+import About from "./About";
 
+import {Switch,BrowserRouter,Route} from "react-router-dom";
 const App = () => {
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
+    <BrowserRouter>
+    
+      <div>
+          <Navigation></Navigation>
+          <Switch>
+            <Route exact path="/" component={Home}></Route>
+            <Route path="/about" component={About}></Route>
+          </Switch>
+      </div>
+    </BrowserRouter>
   )
 }
 
